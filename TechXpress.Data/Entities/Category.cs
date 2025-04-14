428a8fdf221d4f97a58220045e522d0a8dbc3fab
Kerolos Magdy Damian Zaki <kerolosm.demian@gmail.com>
2025-04-14 17:41:19 +0200

    edit category&some fix UI

--- TechXpress.Data/Entities/Category.cs
+++ TechXpress.Data/Entities/Category.cs
@@ -4,7 +4,7 @@ namespace TechXpress.Data.Entities
 {
     public class Category
     {
-        public int Id { get; private set; }
+        public int Id { get;  set; }
         public string? Name { get;  set; } 
         public List<Product> Products { get;  set; } = new();
 
